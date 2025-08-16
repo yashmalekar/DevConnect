71c343ae588bb44fa423ebf430b03dae7855fc6e
Yash Malekar <heramb.malekar@gmail.com>
2025-08-16 17:27:37 +0530

    Modified gitignore for cloudinary config

--- backend/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: 'dhahqd8g5',
-  api_key: '611377117964874',
-  api_secret: 'BytP7wYIDOdSQpuG7McSuU-uxcA'
-});
-
-export default cloudinary;
